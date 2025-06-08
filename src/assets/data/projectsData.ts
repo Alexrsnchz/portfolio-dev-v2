ae5d0e660205c96cb5b8cb693944a6f655900a90
Alexrsnchz <77534895+Alexrsnchz@users.noreply.github.com>
2025-06-08 22:26:32 +0200

    feat: Projects section v2

---
+++ src/assets/data/projectsData.ts
@@ -0,0 +1,74 @@
+const projects = [
+  {
+    image: "/images/mobelia_thumbnail.webp",
+    title: "E-commerce de venta de muebles",
+    description:
+      "Comercio moderno de venta de muebles con diseño responsivo, carrito de compra, pagos seguros y autenticación con sociales.",
+    stack: [
+      "JavaScript",
+      "React",
+      "Tailwind CSS",
+      "Java",
+      "Spring Boot",
+      "PostgreSQL",
+      "Auth0",
+    ],
+    sourceCode: "https://github.com/Alexrsnchz/mobelia-web",
+    preview: "https://mobelia-web.vercel.app/",
+    finished: false,
+  },
+  {
+    image: "/images/project2.png",
+    title: "Asistente botánico",
+    description:
+      "Comercio moderno de venta de muebles con diseño responsivo, carrito de compra, pagos seguros y autenticación con sociales.",
+    stack: [
+      "JavaScript",
+      "React",
+      "Tailwind CSS",
+      "Express.js",
+      "MySQL",
+      "Prisma",
+      "Auth0",
+    ],
+    sourceCode: "https://github.com/Alexrsnchz/mobelia-web",
+    preview: "https://mobelia-web.vercel.app/",
+    finished: true,
+  },
+  {
+    image: "/images/project3.png",
+    title: "Panel de administración",
+    description:
+      "Comercio moderno de venta de muebles con diseño responsivo, carrito de compra, pagos seguros y autenticación con sociales.",
+    stack: [
+      "TypeScript",
+      "React",
+      "Tailwind CSS",
+      "PHP",
+      "Laravel",
+      "PostgreSQL",
+    ],
+    sourceCode: "https://github.com/Alexrsnchz/mobelia-web",
+    preview: "https://mobelia-web.vercel.app/",
+    finished: true,
+  },
+  {
+    image: "/images/project4.png",
+    title: "Recetario de cocina",
+    description:
+      "Comercio moderno de venta de muebles con diseño responsivo, carrito de compra, pagos seguros y autenticación con sociales.",
+    stack: [
+      "JavaScript",
+      "React",
+      "Tailwind CSS",
+      "Express.js",
+      "MySQL",
+      "Auth0",
+    ],
+    sourceCode: "https://github.com/Alexrsnchz/mobelia-web",
+    preview: "https://mobelia-web.vercel.app/",
+    finished: true,
+  },
+];
+
+export default projects;
---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface ProjectProps {
+  image: string;
+  title: string;
+  description: string;
+  stack: string[];
+  sourceCode: string;
+  preview: string;
+  finished: boolean;
+  reverse: boolean;
+}
