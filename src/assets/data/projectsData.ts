11f85ddb9e2bfc9d878a053df4a12cdea01ef7bc
Alexrsnchz <77534895+Alexrsnchz@users.noreply.github.com>
2025-06-14 19:33:24 +0200

    feat: Add responsiveness for the projects section

--- src/assets/data/projectsData.ts
+++ src/assets/data/projectsData.ts
@@ -17,60 +17,58 @@ const projects = [
     preview: "https://mobelia-web.vercel.app/",
     finished: false,
   },
-  /*  
-    {
-      image: "/images/project2.png",
-      title: "Asistente botánico",
-      description:
-        "Comercio moderno de venta de muebles con diseño responsivo, carrito de compra, pagos seguros y autenticación con sociales.",
-      stack: [
-        "JavaScript",
-        "React",
-        "Tailwind CSS",
-        "Express.js",
-        "MySQL",
-        "Prisma",
-        "Auth0",
-      ],
-      sourceCode: "https://github.com/Alexrsnchz/mobelia-web",
-      preview: "https://mobelia-web.vercel.app/",
-      finished: true,
-    },
-    {
-      image: "/images/project3.png",
-      title: "Panel de administración",
-      description:
-        "Comercio moderno de venta de muebles con diseño responsivo, carrito de compra, pagos seguros y autenticación con sociales.",
-      stack: [
-        "TypeScript",
-        "React",
-        "Tailwind CSS",
-        "PHP",
-        "Laravel",
-        "PostgreSQL",
-      ],
-      sourceCode: "https://github.com/Alexrsnchz/mobelia-web",
-      preview: "https://mobelia-web.vercel.app/",
-      finished: true,
-    },
-    {
-      image: "/images/project4.png",
-      title: "Recetario de cocina",
-      description:
-        "Comercio moderno de venta de muebles con diseño responsivo, carrito de compra, pagos seguros y autenticación con sociales.",
-      stack: [
-        "JavaScript",
-        "React",
-        "Tailwind CSS",
-        "Express.js",
-        "MySQL",
-        "Auth0",
-      ],
-      sourceCode: "https://github.com/Alexrsnchz/mobelia-web",
-      preview: "https://mobelia-web.vercel.app/",
-      finished: true,
-    },
-    */
+  {
+    image: "/images/project2.png",
+    title: "Asistente botánico",
+    description:
+      "Comercio moderno de venta de muebles con diseño responsivo, carrito de compra, pagos seguros y autenticación con sociales.",
+    stack: [
+      "JavaScript",
+      "React",
+      "Tailwind CSS",
+      "Express.js",
+      "MySQL",
+      "Prisma",
+      "Auth0",
+    ],
+    sourceCode: "https://github.com/Alexrsnchz/mobelia-web",
+    preview: "https://mobelia-web.vercel.app/",
+    finished: true,
+  },
+  {
+    image: "/images/project3.png",
+    title: "Panel de administración",
+    description:
+      "Comercio moderno de venta de muebles con diseño responsivo, carrito de compra, pagos seguros y autenticación con sociales.",
+    stack: [
+      "TypeScript",
+      "React",
+      "Tailwind CSS",
+      "PHP",
+      "Laravel",
+      "PostgreSQL",
+    ],
+    sourceCode: "https://github.com/Alexrsnchz/mobelia-web",
+    preview: "https://mobelia-web.vercel.app/",
+    finished: true,
+  },
+  {
+    image: "/images/project4.png",
+    title: "Recetario de cocina",
+    description:
+      "Comercio moderno de venta de muebles con diseño responsivo, carrito de compra, pagos seguros y autenticación con sociales.",
+    stack: [
+      "JavaScript",
+      "React",
+      "Tailwind CSS",
+      "Express.js",
+      "MySQL",
+      "Auth0",
+    ],
+    sourceCode: "https://github.com/Alexrsnchz/mobelia-web",
+    preview: "https://mobelia-web.vercel.app/",
+    finished: true,
+  },
 ];
 
 export default projects;
